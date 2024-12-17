1c309fc89dd8165160fe27ffc10ba256816b199b
Tresordie <Tresordie@163.com>
2024-12-17 21:58:06 +0800

    combined all case into one python script to parse all kinds of SCUD CAN log file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # parse_can_frame_via_dbc
-parse CAN data frame according to dbc file
+parse SCUD CAN data log according to dbc file
+
+# Notes
+1. generally SCUD used 2 types of application to capture CAN log
+    - CANTest
+    - CANas
+2. SCUD captured CAN logs used different encoded csv file
+    - GB2312 code
+    - UTF8 code
+3. SCUD captured CAN logs sometimes used different csv title row(head row)
+
+# Usage
+1. please install library of python(pandas, chardet, tqdm, cantools)
+    pip install pandas
+    pip install chardet
+    pip install tqdm
+    pip install cantools
+
+2. make sure your .dbc file in the file path you specified
+
+3. make sure CAN log csv files in the file path you specified
+
+4. confirm with SCUD that what kind of CAN application tool used for CAN log capture(CANTest? CANas?)
+
+5. sometimes SCUD split CAN logs to several parts because long time capture, we need to combine all parts CAN logs to one
+
+6. how many CAN logs captured need to be combined as one, add these CAN log csv files name into the raw_log_list. for example, I want to combine 'pack1.csv', 'pack2.csv', 'pack3.csv' in current folder
+    raw_log_list = [
+        "pack1.csv",
+        'pack2.csv',
+        'pack3.csv',
+    ]
+
+7. create a class for can log parse, for example as below:
+    scud_can_log_convert = scud_can_log_convert(
+        dbc_file_full_path="./maple_202424100.dbc",     # specify dbc file path used
+        raw_log_file_path="./",                         # CAN log folder path(e.g., current folder)
+        raw_logs_list=raw_log_list,                     # CAN log csv files list which we need to combined(it can be only one CAN log)
+        file_name_combined_logs="pack_cb.csv",          # specify combined CAN log file name
+        file_path_parsed="./pack_PARSED.csv",           # parsed CAN log file path
+        can_application_tool="CANas",                   # CAN application tool used (CANTest, CANas), time stamp different
+    )
--- can_parse_via_dbc.py
+++ can_parse_via_dbc.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 """
 @File    :   can_parse_via_dbc.py
-@Time    :   2024/06/02 20:31:29
-@Author  :   SimonYuan 
+@Time    :   2024/12/17 21:46:55
+@Author  :   SimonYuan
 @Version :   1.0
 @Site    :   https://tresordie.github.io/
-@Desc    :   convert scud can log, porting from Ion Buzdugan's script, fixed the bug "Unable to convert"
+@Desc    :   
+1. convert scud can log, porting from Ion Buzdugan's script, fixed the issue that standard frame "Unable to convert"
+2. covered almost all kinds of SCUD CAN log files format so far
 """
 
 import cantools
-import pandas as pd
-import can
+import chardet
 from csv_operate import *
 from tqdm import tqdm
 
@@ -23,6 +24,7 @@ class scud_can_log_convert(object):
         raw_logs_list=None,
         file_name_combined_logs=None,
         file_path_parsed=None,
+        can_application_tool="CANTest",
     ):
         self.dbc_file_full_path = dbc_file_full_path
 
@@ -35,48 +37,53 @@ class scud_can_log_convert(object):
         )
 
         self.file_path_parsed = file_path_parsed
+        self.can_application_tool = can_application_tool
+
         self.dbc_message_list = []
         self.can_id_parsed_msg_dict = {}
         self.can_id_parsed_msg_dict_list = []
 
+    def detect_encoding(self):
+        with open(self.raw_log_file_path + self.raw_logs_list[0], "rb") as f:
+            data = f.read(1024)  # read 1KB data
+        result = chardet.detect(data)
+        encoding = result["encoding"]
+        return encoding
+
     # combined all raw logs to one
     def multiple_scud_logs_combine(self):
-        combine_gb2312_csv_log_files(
-            # combine_utf8_csv_log_files(
-            self.raw_log_file_path,
-            self.raw_logs_list,
-            self.file_name_combined_logs,
-        )
+        """
+        1. sometimes REL logged multiple CAN log csv files, combine them into one
+        2. SCUD captured CAN log in GB2312 or UTF8 format, please confirm by yourself
+        """
+        encoding = self.detect_encoding()
+
+        if encoding == "GB2312" or encoding == "GBK":
+            print("file encoing with GB2312(or GBK)")
+            combine_gb2312_csv_log_files(
+                self.raw_log_file_path,
+                self.raw_logs_list,
+                self.file_name_combined_logs,
+            )
+        elif encoding == "UTF-8":
+            print("file encoing with UTF-8")
+            combine_utf8_csv_log_files(
+                self.raw_log_file_path,
+                self.raw_logs_list,
+                self.file_name_combined_logs,
+            )
+        elif encoding == "UTF-8-SIG":
+            print("file encoing with UTF-8-SIG")
+            combine_utf8_sig_csv_log_files(
+                self.raw_log_file_path,
+                self.raw_logs_list,
+                self.file_name_combined_logs,
+            )
+        else:
+            print("file encoing uncertain!")
 
     # convert Chinese to English
     def convert_can_log_to_en(self):
-        df = pd.read_csv(self.full_path_combined_logs)
-
-        for i in tqdm(range(df.shape[0])):
-            if df.loc[i, "传输方向"] == "接收":
-                df.loc[i, "传输方向"] = "Receive"
-
-            # SCUD每次抓的CAN log标题定义不一致
-            # if df.loc[i, "帧格式"] == "数据帧":
-            #     df.loc[i, "帧格式"] = "Data"
-
-            if df.loc[i, "帧格式"] == "标准帧":
-                df.loc[i, "帧格式"] = "Standard"
-
-            # if df.loc[i, "帧类型"] == "标准帧":
-            #     df.loc[i, "帧类型"] = "Standard"
-
-            if df.loc[i, "帧类型"] == "数据帧":
-                df.loc[i, "帧类型"] = "Data"
-
-            # if df.loc[i, "帧类型"] == "扩展帧":
-            #     df.loc[i, "帧类型"] = "Extended"
-
-            if df.loc[i, "帧格式"] == "扩展帧":
-                df.loc[i, "帧格式"] = "Extended"
-
-        df.to_csv(self.full_path_combined_logs, index=False)
-
         df = pd.read_csv(self.full_path_combined_logs, header=None)
         for j in tqdm(range(df.shape[1])):
             if df.iloc[0, j] == "序号":
@@ -99,25 +106,55 @@ class scud_can_log_convert(object):
                 df.iloc[0, j] = "Data Length"
             elif df.iloc[0, j] == "数据(HEX)" or df.iloc[0, j] == "数据":
                 df.iloc[0, j] = "Data(HEX)"
-
         df.to_csv(self.full_path_combined_logs, index=False, header=None)
 
-    # todo: 根据dbc解析scud can log
+        df = pd.read_csv(self.full_path_combined_logs)
+        for i in tqdm(range(df.shape[0])):
+            if df.loc[i, "Direction"] == "接收":
+                df.loc[i, "Direction"] = "Receive"
+
+            # SCUD captured CAN logs used different format csv file titles
+            if df.loc[i, "Format"] == "数据帧":
+                df.loc[i, "Format"] = "Data"
+
+            if df.loc[i, "Format"] == "标准帧":
+                df.loc[i, "Format"] = "Standard"
+
+            if df.loc[i, "Format"] == "扩展帧":
+                df.loc[i, "Format"] = "Extended"
+
+            if df.loc[i, "Type"] == "标准帧":
+                df.loc[i, "Type"] = "Standard"
+
+            if df.loc[i, "Type"] == "数据帧":
+                df.loc[i, "Type"] = "Data"
+
+            if df.loc[i, "Type"] == "扩展帧":
+                df.loc[i, "Type"] = "Extended"
+        df.to_csv(self.full_path_combined_logs, index=False)
+
     def parse_scud_can_log(self):
+        """
+        parse SCUD CAN log via corresponding dbc file
+        """
         db = cantools.database.load_file(self.dbc_file_full_path)
         df = pd.read_csv(self.full_path_combined_logs)
 
         df = df[df["Direction"] == "Receive"]
-        df["Time Stamp"] = df["Time Stamp"].str[:-2]
-        df["Time Stamp"] = pd.to_datetime(df["Time Stamp"], format="%H:%M:%S.%f")
 
-        # 在原有data frame的基础上添加了df['Test Time (ms)']的新列，获取以ms为单位的时间，在最后添加了新列 ‘Test Time (ms)’
-        # astype进行数据类型转换
-        df["Test Time (ms)"] = (df["Time Stamp"] - df["Time Stamp"].iloc[0]).astype(
-            "timedelta64[ms]"
-        )
+        # when SCUD used CANTest application, time stamp format is "14:29:57.690.0"
+        if self.can_application_tool == "CANTest":
+            df["Time Stamp"] = df["Time Stamp"].str[:-2]
+            df["Time Stamp"] = pd.to_datetime(df["Time Stamp"], format="%H:%M:%S.%f")
+            df["Test Time (ms)"] = (
+                df["Time Stamp"] - df["Time Stamp"].iloc[0]
+            ).dt.total_seconds() * 1000
+        elif self.can_application_tool == "CANas":
+            # when SCUD used CANas application, time stamp format is "2924.7805"(unit is Second)
+            df["Time Stamp"] = df["Time Stamp"] * 1000
+            df["Test Time (ms)"] = df["Time Stamp"] - df["Time Stamp"].iloc[0]
 
-        # 将dbc文件中所有的messages的name组成一个list赋值给messages
+        # all dbc messages list as title of parsed csv file
         self.dbc_message_list = [msg.name for msg in db.messages]
         df_out = pd.DataFrame(columns=["Test Time (ms)"] + self.dbc_message_list)
 
@@ -128,18 +165,12 @@ class scud_can_log_convert(object):
                     data=bytearray.fromhex(df["Data(HEX)"].iloc[row]),
                 )
 
-                # 1. 如果decode_message失败，就不能添加时间
-                # 2. 如果先添加时间到，decode_message内容赋值会覆盖Test Time (ms)的数据
+                # 1. can't add test_time(ms) if failed to decode message
+                # 2. test_time(ms) should be added at last, or it will be overwritten by other info of decode message
                 self.can_id_parsed_msg_dict["Test Time (ms)"] = df[
                     "Test Time (ms)"
                 ].iloc[row]
-
-                # print('can_id_parsed_msg_dict: row = %d' % row)
-                # print(self.can_id_parsed_msg_dict)
                 self.can_id_parsed_msg_dict_list.append(self.can_id_parsed_msg_dict)
-                # print('can_id_parsed_msg_dict_list: row = %d' % row)
-                # print(self.can_id_parsed_msg_dict_list)
-
             except:
                 frame_id = df["Frame ID"].iloc[row]
                 print(f"Unable to convert {frame_id}")
@@ -152,36 +183,23 @@ class scud_can_log_convert(object):
 
 
 if __name__ == "__main__":
-    # 指定要合并的文件名列表，无需指令路径
+    # CAN log csv files list need to be combined
     raw_log_list = [
         "COT_Pack.csv",
         # 'pack2.csv',
         # 'pack3.csv',
-        # 'pack4.csv',
-        # 'pack5.csv',
     ]
 
+    # fmt: off
     scud_can_log_convert = scud_can_log_convert(
-        # 指定用于解析的DBC文件的路径
-        dbc_file_full_path="./maple.dbc",
-        # 指定需要解析的原始CAN log的路径
-        raw_log_file_path="./",
-        # 指定需要解析的原始CAN log文件名列表
-        raw_logs_list=raw_log_list,
-        # 当有多个文件需要合并时，这里指定合并之后的文件名(为了兼容单个文件的情况，也需要手动来制定)
-        file_name_combined_logs="pack_cb.csv",
-        # 指定解析文件路径及名称
-        file_path_parsed="./pack_PARSED.csv",
+        dbc_file_full_path="./maple_202424100.dbc",     # specify dbc file used
+        raw_log_file_path="./",                         # CAN log folder path
+        raw_logs_list=raw_log_list,                     # CAN log csv files list
+        file_name_combined_logs="pack_cb.csv",          # specify combined CAN log file name
+        file_path_parsed="./pack_PARSED.csv",           # parsed CAN log file path
+        can_application_tool="CANas",                   # CAN application tool used (CANTest, CANas), time stamp different
     )
-
-    # 多个raw CAN logs文件合并,由于SCUD提供raw CAN log的csv文件都是gb2312进行编码的, 读取gb2312编码的CAN log且合并之后会转存为utf-8格式csv
-    # 如raw CAN logs不是gb2312编码, 请自行修改代码
+    # fmt: on
     scud_can_log_convert.multiple_scud_logs_combine()
-
-    # 多文件(utf-8)合并后，将所有的英文翻译成英文
     scud_can_log_convert.convert_can_log_to_en()
-
-    # 将合并之后的CAN log csv文件(utf-8)解析出来
-    # 解析部分的代码源自于Ion的代码，修复了其代码的bug(很多无法解析的情况)
-    # 修复之后的代码所有的标准帧都可以解析，扩展帧无法解析
     scud_can_log_convert.parse_scud_can_log()
